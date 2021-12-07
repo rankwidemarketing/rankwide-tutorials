36447343af1a345ed09bd3cc015f67386bb47371
rankwidemarketing <95674925+rankwidemarketing@users.noreply.github.com>
2021-12-07 12:10:56 +0530

    Created single-content.php

---
+++ content-single.php
@@ -0,0 +1,17 @@
+<section class="rw-author-box">
+  <div class="rw-author-box-wrap">
+    <div class="rw-author-box-image-wrap">
+      <div class="rw-author-box-image">
+        <img src="https://thecinemaholic.com/wp-content/themes/cinemaholic/author.jpg" width="100px" alt="Rankwide Author">
+      </div>
+    </div>
+    <div class="rw-author-content">
+      <div class="rw-author-content-title">
+        <h3>Mithun Baiju</h3>
+      </div>
+      <div class="rw-author-content-description">
+        <p>I am a professional WordPress developer who loves building with WordPress. If not building website, you can find me geeking around marketing forums and testing new SEO strategies.</p>
+      </div>
+    </div>
+  </div>
+</section>
